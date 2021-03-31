57d649938b604965c29e8134ec09d7dd6fbbc341
Artyom Yeremenko <eremenko_aa@groupbwt.com>
2021-03-31 16:52:30 +0300

    fix: bug in transaction collection

--- src/Collection/TransactionCollection.php
+++ src/Collection/TransactionCollection.php
@@ -26,18 +26,32 @@ class TransactionCollection
             $this->push($transaction);
         }
     }
-
+    
+    /**
+     * Get all user transactions
+     * @param User $user
+     *
+     * @return array
+     */
     public function allByUser(User $user): array
     {
         return $this->transactions[$user->getId()] ?? [];
     }
-
+    
+    /**
+     * Get all user transactions with specific transaction type
+     *
+     * @param User   $user
+     * @param string $type transaction type
+     *
+     * @return array
+     */
     public function allByUserTransactionType(User $user, string $type): array
     {
         $transactions = $this->transactions[$user->getId()] ?? [];
 
         return array_filter($transactions, function (Transaction $transaction) use ($type) {
-            return $transaction->getType() === $type;
+            return $transaction->getType()->getName() === $type;
         });
     }
 
